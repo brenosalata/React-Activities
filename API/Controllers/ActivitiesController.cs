1b34754e80432b11ee1aa05fd8d4d3f3943ed241
salatabreno1 <you@example.com>
2023-06-01 14:48:16 -0300

    end of section 4

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Application.Activities;
 using Domain;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Persistence;
 
@@ -10,23 +12,35 @@ namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-        private readonly DataContext _context;
-        public ActivitiesController(DataContext context)
-        {
-            _context = context;
-            
-        }
-
         [HttpGet] //api//activities
-        public ActionResult<List<Activity>> GetActivities()
+        public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return  _context.Activities.ToList();
+            return await Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")] //api/activities/id
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            return await _context.Activities.FindAsync(id);
+            return await Mediator.Send(new Details.Query{Id = id});
+        }
+
+        [HttpPost] //api/activities/activity
+        public async Task<IActionResult> CreateActivity(Activity activity)
+        {
+            return Ok(await Mediator.Send(new Create.Command { Activity = activity}));
+        }
+
+        [HttpPut("{id}")] //api/activities/activity
+        public async Task<IActionResult> EditActivity(Guid id, Activity activity)
+        {
+            activity.Id = id;
+            return Ok(await Mediator.Send(new Edit.Command { Activity = activity}));
+        }
+
+        [HttpDelete("{id}")] //api/activities/{id}
+        public async Task<IActionResult> DeleteActivity(Guid id)
+        {
+            return Ok(await Mediator.Send(new Delete.Command { Id = id}));
         }
     }
 }
