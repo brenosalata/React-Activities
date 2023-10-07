e7d651e3d5aef0b2dd01ca6668133c4390126ea3
salatabreno1 <you@example.com>
2023-10-07 14:44:46 -0300

    begin with section 17

---
+++ client-app/src/features/profiles/ProfileHeader.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Grid, Segment } from 'semantic-ui-react';
+
+export default function ProfileHeader() {
+    return (
+        <Segment>
+            <Grid>
+                
+            </Grid>
+        </Segment>
+    )
+}
